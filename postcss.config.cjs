6d613d7db2e7b8dc4c714185948f9d4e25ae607f
lchylik <lukas.chylik@ais-servis.cz>
2024-02-13 15:35:28 +0100

    fix

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('cssnano'),
-    require('postcss-preset-env')({
-      stage: 3,
-    }),
+    require('cssnano')
   ],
 };
