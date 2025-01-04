a7f68b34de8da5418bd3246de62adcba606731f3
Lukáš Chylík <lukas.chylik@riganti.cz>
2025-01-04 15:35:21 +0100

    update header

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,7 +4,7 @@ import type { NavigationProps } from './navigatioin.types';
 
 const Navigation = component$<NavigationProps>(({ small, links }) => {
   return (
-    <nav data-small={small} class="fixed inset-x-1/2 top-0 grid place-items-center w-full translate-x-[-50%] text-white z-1000">
+    <nav data-small={small} class="fixed inset-x-1/2 top-0 grid place-items-center w-full translate-x-[-50%] text-white z-50">
       <ul class="inline-flex justify-center gap-4 list-none m-0 translate-y-[5svb] p-0 py-6 px-8 rounded-full bg-[hsla(0,0%,30%,0.5)] backdrop-blur-[8px]">
         {links && links.map((link: LinkProps, index: number) => (
           <li key={index} class="px-2 transition-transform duration-300 hover:scale-110">
--- src/sections/intro/intro.tsx
+++ src/sections/intro/intro.tsx
@@ -11,7 +11,7 @@ const Intro = component$(() => {
             <strong class="block mb-4 tracking-widest text-[clamp(1.5rem,3.5svb,2.5rem)]">
               Creative web developer
             </strong>
-            <h1 class="text-[clamp(2rem,10svb,8rem)] font-bold uppercase leading-medium tracking-widest intro__title">
+            <h1 class="text-[clamp(2rem,10svb,8rem)] font-bold uppercase leading-normal tracking-widest intro__title">
               Lukáš Chylík
             </h1>
             <Button
@@ -27,7 +27,8 @@ const Intro = component$(() => {
           <div>
             <img
               src="https://lukaschylik.sk/wwwroot/images/meta/meta-image.jpg"
-              alt="Profile photo of me"
+              alt="Profile photo of Lukáš Chylík"
+              title="Profile photo of Lukáš Chylík"
               width="1600"
               height="1600"
               class="block max-w-full h-auto rounded-full shadow-[0_0_10px_rgba(0,0,0,0.5)] grayscale opacity-80"
