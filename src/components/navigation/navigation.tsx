5e94c12da2ae3360c18b180d68a5d28c312830fe
Lukáš Chylík <lukas.chylik@riganti.cz>
2025-01-26 12:05:31 +0100

    Add mobile menu toggle functionality and enhance navigation styles

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,17 +1,34 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore, $ } from "@builder.io/qwik";
 import { Link, type LinkProps } from "@builder.io/qwik-city";
 import type { NavigationProps } from "./navigatioin.types";
 import Social from "../social/social";
 
 const Navigation = component$<NavigationProps>(({ links }) => {
+  const state = useStore({ isOpen: false });
+
+  const toggleMenu = $(() => {
+    state.isOpen = !state.isOpen;
+  });
+
   return (
     <>
-      <nav class="nav fixed top-0 text-center py-8 px-1 w-full z-50 backdrop-blur-md">
-        <ul class="nav__list inline-flex justify-center opacity-0 gap-12 list-none m-0">
+      <nav
+        class="nav fixed top-0 flex justify-between text-center py-8 px-1 w-full z-50 px-32"
+        data-open={state.isOpen}>
+        <a
+          href="/"
+          role="menuitem"
+          class="nav__logo text-white text-2xl uppercase no-underline decoration-secondary decoration-5 underline-offset-8 hover:underline">
+          Home
+        </a>
+        <ul
+          class="nav__list inline-flex justify-center align-baseline items-start opacity-0 gap-12 list-none m-0"
+          role="menu">
           {links &&
             links.map((link: LinkProps, index: number) => (
               <li key={index}>
                 <Link
+                  role="menuitem"
                   href={link.href}
                   title={link.text}
                   rel="internal"
@@ -21,6 +38,13 @@ const Navigation = component$<NavigationProps>(({ links }) => {
               </li>
             ))}
         </ul>
+
+        <button
+          type="button"
+          class="nav__toggler w-16 h-16 bg-secondary text-white cursor-pointer"
+          aria-label={state.isOpen ? "Zavrieť menu" : "Otvoriť menu"}
+          title={state.isOpen ? "Zavrieť menu" : "Otvoriť menu"}
+          onClick$={toggleMenu}></button>
       </nav>
       <Social />
     </>
