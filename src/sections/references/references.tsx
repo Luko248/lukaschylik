526786fc51a44aedc855c4f6f1a5bf3a10703430
Lukas Chylik <chylik.lukas@gmail.com>
2025-06-16 21:14:36 +0200

    feat: add video carousel to references section with scrollbar styling

--- src/sections/references/references.tsx
+++ src/sections/references/references.tsx
@@ -13,19 +13,20 @@ const References = component$(() => {
           <div class="relative xl:sticky xl:top-0 xl:h-fit mb-8 xl:mb:0">
             <SectionTitle text="Referencie" />
             <div class="content-fade-in">
-              <p class="text-l lg:text-xl leading-relaxed font-light mb-2 text-gray-300 font-mono">
+              <p class="text-l lg:text-xl leading-relaxed font-light mb-8 text-gray-300 font-mono">
                 Od roku 2016 som sa venoval tvorbe webových stránok pre klientov
                 ako Elendris alebo Anew Style.
               </p>
-              <p class="text-l lg:text-xl leading-relaxed font-light mb-2 text-gray-300 font-mono">
+              <p class="text-l lg:text-xl leading-relaxed font-light mb-8 text-gray-300 font-mono">
                 Mal som dlhodobú spoluprácu s Riganti, kde som prispieval k
                 rôznym webovým projektom. Pre Update Conference som vytvoril
                 množstvo webových stránok a pomohol vyvinúť UI kit pre DotVVM
                 framework. Už nespolupracujem s Riganti.
               </p>
-              <p class="text-l lg:text-xl leading-relaxed font-light mb-20 text-gray-300 font-mono">
-                Momentálne pracujem pre AIS Servis, kde vyvíjam dizajnový systém
-                pre VIG Česká republika.
+
+              <p class="text-l lg:text-xl leading-relaxed font-light mb-16 text-gray-300 font-mono">
+                Okrem vývoja sa venujem aj prednášaniu na rôznych meetupoch a
+                konferenciách.
               </p>
               <Button
                 variant="secondary"
@@ -34,6 +35,28 @@ const References = component$(() => {
                 href="#contact">
                 Služby a spoupráca
               </Button>
+              <div class="video-carousel flex flex-row gap-4 mt-20 snap-x snap-mandatory max-w-[560px] overflow-auto">
+                <iframe
+                  class="snap-center min-w-full"
+                  loading="lazy"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/hwhuSdO5fU0?si=ZckQ-cJlCGZpJExv"
+                  title="YouTube video Lukáš Chylík - Frontendové novinky"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullscreen></iframe>
+                <iframe
+                  class="snap-center min-w-full"
+                  loading="lazy"
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/p8sQyt4YFdk?si=WDxTC4029EeFecKk"
+                  title="YouTube video Lukáš Chylík - Pozicování pomocí CSS anchor"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullscreen></iframe>
+              </div>
             </div>
           </div>
           <ul class="ref-list list-none p-0 m-0 content-fade-in xl:mt-5 3xl:mt-8">
