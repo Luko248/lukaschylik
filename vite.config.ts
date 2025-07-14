d9d7aaf86c78539ebdfd519ea6f48407b54b34e8
Lukas Chylik <chylik.lukas@gmail.com>
2025-07-14 16:24:50 +0200

    fix: add display contents and tabIndex to heading anchors for better accessibility

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig((): UserConfig => {
                 behavior: "wrap",
                 properties: {
                   className: ["heading-anchor"],
+                  tabIndex: 0,
                 },
               },
             ],
