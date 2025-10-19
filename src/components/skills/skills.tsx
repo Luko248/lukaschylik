9be4306a42371a503e434ba9af2bdeed5d256d4f
Lukas Chylik <chylik.lukas@gmail.com>
2025-10-19 18:07:47 +0200

    style: remove redundant xl breakpoint classes from skills component spacing

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -27,13 +27,13 @@ const Skills = component$(() => {
   ];
 
   return (
-    <div class="skills relative z-20 grid gap-y-4 lg:gap-y-5 xl:gap-y-5 2xl:gap-y-8 overflow-clip bg-white dark:bg-black">
-      <ul class="flex flex-row flex-nowrap gap-x-4 lg:gap-x-5 xl:gap-x-5 2xl:gap-x-8 overflow-clip skills__scroller text-black dark:text-white">
+    <div class="skills relative z-20 grid gap-y-4 lg:gap-y-5 2xl:gap-y-8 overflow-clip bg-white dark:bg-black">
+      <ul class="flex flex-row flex-nowrap gap-x-4 lg:gap-x-5 2xl:gap-x-8 overflow-clip skills__scroller text-black dark:text-white">
         {skills.map((skill) => (
           <SkillItem key={skill} text={skill} />
         ))}
       </ul>
-      <ul class="flex flex-row-reverse flex-nowrap gap-x-4 lg:gap-x-5 xl:gap-x-5 2xl:gap-x-8 overflow-clip skills__scroller text-black dark:text-white">
+      <ul class="flex flex-row-reverse flex-nowrap gap-x-4 lg:gap-x-5 2xl:gap-x-8 overflow-clip skills__scroller text-black dark:text-white">
         {skills.map((skill) => (
           <SkillItem key={skill} text={skill} />
         ))}
